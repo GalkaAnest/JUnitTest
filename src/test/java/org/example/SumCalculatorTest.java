e18f7a7e2f3ff2bae17aa48cafabc8dcbef240ca
Galyna Gaponova <galyna.gaponova@kyrylos-mbp.lan>
2023-11-30 14:55:51 -0500

    first commit

---
+++ src/test/java/org/example/SumCalculatorTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumCalculatorTest {
+   private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void setUp() {
+        sumCalculator = new SumCalculator();
+    }
+
+    @Test
+    public void testSumOne() {
+        int result = sumCalculator.sum(1);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testSumThree() {
+        int result = sumCalculator.sum(3);
+        assertEquals(6, result);
+    }
+
+
+    @Test
+    public void testSumZeroException() throws IllegalArgumentException {
+        int sum = sumCalculator.sum(0);
+        if (sum == 0) {
+            System.out.println("IllegalArgumentException");
+        }
+
+    }
+}
